2d4821a3edcca9e09318f9e3351af8b83b01e25b
AradoBlitz <schlachtnavaho@gmail.com>
2016-02-25 01:42:21 +0300

    Moved shufl method and inpudPasswordList field with code inizialization to PasswordListGenerator class.

--- concurrent.producer.consumer/src/concurrent/producer/consumer/HashedPasswordHackerTest.java
+++ concurrent.producer.consumer/src/concurrent/producer/consumer/HashedPasswordHackerTest.java
@@ -15,17 +15,13 @@ public class HashedPasswordHackerTest {
 
 	private static final String password = "#edPass1";
 	private static final String passwordsHash = "6c8e69629ff154ec36a507118e549f97";
-	private List<String> inputPasswordList = asList(shufl(password)
-			,shufl(password)
-			,shufl(password)
-			,password
-			,shufl(password));
+	
 
 	@Test
 	public void findPasswordByHesh() throws Exception {
 		
 		assertThat(new PasswordsHashMatcher(
-				new PasswordListGenerator(inputPasswordList))
+				new PasswordListGenerator(password))
 				.find(passwordsHash)
 				, equalTo(password));
 	}
@@ -35,23 +31,11 @@ public class HashedPasswordHackerTest {
 		assertThat(password ,equalTo("#edPass1"));
 		assertThat(Encoder.md5(password),equalTo(passwordsHash));
 		//Сгенерировать список паролей для перебора на основе правильного перебора с помощью случайных перестановок
-		assertThat(shufl(password),is(not(equalTo(password))));
+		assertThat(PasswordListGenerator.shufl(password),is(not(equalTo(password))));
 		
 	}
 	
-	 private String shufl(String password) {
-		String shufle = password;
-		Random rn = new Random();
-		char[] buf = password.toCharArray();
-		for(int i = password.length()-1;i>0;i--){
-				int selected = rn.nextInt(i);
-				char smb = buf[i];
-				buf[i] = buf[selected];
-				buf[selected]=smb;
-		}	
-		System.out.println(buf);
-		return new String(buf);
-	}
+	
 
 	
 }
--- concurrent.producer.consumer/src/concurrent/producer/consumer/PasswordListGenerator.java
+++ concurrent.producer.consumer/src/concurrent/producer/consumer/PasswordListGenerator.java
@@ -1,13 +1,22 @@
 package concurrent.producer.consumer;
 
+import static java.util.Arrays.asList;
+
 import java.util.List;
+import java.util.Random;
 
 public class PasswordListGenerator {
 
 	private List<String> inputPasswordList;
 
-	public PasswordListGenerator(List<String> inputPasswordList) {
-		this.inputPasswordList = inputPasswordList;
+	
+	public PasswordListGenerator(String password) {
+
+		inputPasswordList = asList(shufl(password)
+				,shufl(password)
+				,shufl(password)
+				,password
+				,shufl(password));
 	}
 
 	public List<String> getSelectedPasswordsList() {
@@ -15,4 +24,17 @@ public class PasswordListGenerator {
 		return inputPasswordList;
 	}
 
+	 public static String shufl(String password) {
+			String shufle = password;
+			Random rn = new Random();
+			char[] buf = password.toCharArray();
+			for(int i = password.length()-1;i>0;i--){
+					int selected = rn.nextInt(i);
+					char smb = buf[i];
+					buf[i] = buf[selected];
+					buf[selected]=smb;
+			}	
+			System.out.println(buf);
+			return new String(buf);
+		}
 }
